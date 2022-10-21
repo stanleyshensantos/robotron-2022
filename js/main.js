360bc98028bdd0d87ef71700555f2a07ba7de1fa
Staney Shen <stanley_shen@hotmail.com>
2022-10-20 21:50:58 -0300

    https://github.com/stanleyshensantos/robotron-2022

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-document.querySelector("#robotron").addEventListener("click", );
-function dizOi(){
-    console.log('oi')
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+    
+})
+
+
+function manipulaDados(operacao){
+    if(operacao === "-"){
+        braco.value = parseInt(braco.value) - 1
+    }else{
+        braco.value = parseInt(braco.value) + 1
+    }
 }
