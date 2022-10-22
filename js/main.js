d24f1e04a535f9a25d6ec06a984527abca34f24c
Staney Shen <stanley_shen@hotmail.com>
2022-10-21 22:31:17 -0300

    https://github.com/stanleyshensantos/robotron-2022

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
+
 const braco = document.querySelector("#braco")
 
 const controle = document.querySelectorAll(".controle-ajuste")
