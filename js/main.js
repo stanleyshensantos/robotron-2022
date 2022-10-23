feb59fc9f1bb06cbf756e33fedece234721f3839
Staney Shen <stanley_shen@hotmail.com>
2022-10-22 21:10:00 -0300

    https://github.com/stanleyshensantos/robotron-2022

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 
 
 
-const controle = document.querySelectorAll(".controle-ajuste")
+const controle = document.querySelectorAll("[data-controle]")
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
     
 })
 
 
-function manipulaDados(operacao){
-   const peca =document.querySelector("peca")
-    if(operacao === "-"){
+function manipulaDados(operacao, controle){
+   const peca = controle.querySelector("[data-contador]")
+    
+   if(operacao === "-"){
         peca.value = parseInt(peca.value) - 1
     }else{
         peca.value = parseInt(peca.value) + 1
